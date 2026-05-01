aaabb56172c49f93d9fd9a8cd64aff4d425f553f
YouthGrowingJourney <ReaxerYT@outlook.com>
2026-05-01 11:33:33 +0200

    Add products array with initial product data

---
+++ products.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    id: 1,
+    name: "Discipline Guide",
+    price: 19.99,
+    image: "assets/images/product1.jpg"
+  },
+  {
+    id: 2,
+    name: "Habit Tracker",
+    price: 9.99,
+    image: "assets/images/product2.jpg"
+  }
+];
